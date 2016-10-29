60d554d3b97063430fcd889f44ff56c1f850137b
Matt Hartley <m4tthartley@gmail.com>
2016-10-29 02:01:12 +0100

    3D!

--- software_renderer.cc
+++ software_renderer.cc
@@ -37,6 +37,12 @@ union Vec3 {
 	};
 };
 
+struct EulerAngle {
+	float x;
+	float y;
+	float z;
+};
+
 struct IVec2 {
 	int x;
 	int y;
@@ -178,11 +184,6 @@ void Start (State *state) {
 	state->lastTime = GetSeconds();
 }
 
-/*void GetPixelCoords (State *state, float x, float y, int *outX, int *outY) {
-	*outX = (x/2.0f + 0.5f) * state->backBufferSize.x;
-	*outY = (y/2.0f + 0.5f) * state->backBufferSize.y;
-}*/
-
 Vec2 GetPixelCoords (State *state, float x, float y) {
 	Vec2 result;
 	result.x = (x/2.0f + 0.5f) * state->backBufferSize.x;
@@ -203,11 +204,11 @@ void OrthoProjection (State *state, Vec3 *v) {
 	int x = 0;
 }
 
-void PerspectiveProjection (State *state, Vec3 *v, float fov) {
+void PerspectiveProjection (State *state, Vec3 *vec, float fov) {
 	float f = 1.0f / tanf(Rads(fov) / 2.0f);
-	float near = -1.0f;
-	float far = 1.0f;
-	float aspect = state->backBufferSize.x/state->backBufferSize.y;
+	float near = -10.0f;
+	float far = 10.0f;
+	float aspect = (float)state->backBufferSize.x/(float)state->backBufferSize.y;
 	float m[] = {
 		f / aspect, 0, 0, 0,
 		0, f, 0, 0,
@@ -215,13 +216,15 @@ void PerspectiveProjection (State *state, Vec3 *v, float fov) {
 		0, 0, (2.0f * far * near) / (near - far), 0,
 	};
 
+	Vec3 backup = *vec;
 	float w = 0.0f;
-	// v->x = m[0]*v->x + m[1]*v->y + m[2]*v->z + w*m[3];
-	// v->y = m[4]*v->x + m[5]*v->y + m[6]*v->z + w*m[7];
-	// v->z = m[8]*v->x + m[9]*v->y + m[10]*v->z + w*m[11];
-	v->x = m[0]*v->x + m[4]*v->y + m[8]*v->z;
-	v->y = m[1]*v->x + m[5]*v->y + m[9]*v->z;
-	v->z = m[2]*v->x + m[6]*v->y + m[10]*v->z;
+
+	vec->x = m[0]*backup.x + m[1]*backup.y + m[2]*backup.z;
+	vec->y = m[4]*backup.x + m[5]*backup.y + m[6]*backup.z;
+	vec->z = m[8]*backup.x + m[9]*backup.y + m[10]*backup.z;
+
+	vec->x *= 1.0f / backup.z;
+	vec->y *= 1.0f / backup.z;
 }
 
 #define VideoMemoryOffset(coordX, coordY) ((coordY)*state->backBufferSize.x + (coordX))
@@ -265,7 +268,7 @@ float TriangleArea (Vec2 v0, Vec2 v1, Vec2 v2) {
 	}
 }
 
-void RasterizeTriangle (State *state, float *verts, int vectorSize, int count, float *colors, float rotation = 0.0f) {
+void RasterizeTriangle (State *state, float *verts, int vectorSize, int count, float *colors) {
 	Vec3 *v = (Vec3*)verts;
 	Color *c = (Color*)colors;
 
@@ -326,53 +329,22 @@ void RasterizeTriangle (State *state, float *verts, int vectorSize, int count, f
 						if (state->video[VideoMemoryOffset(x, y)].b > 1.0f) state->video[VideoMemoryOffset(x, y)].b = 1.0f;
 						if (state->video[VideoMemoryOffset(x, y)].a > 1.0f) state->video[VideoMemoryOffset(x, y)].a = 1.0f;
 
-						/*int videoOffset = VideoMemoryOffset(x, y);
-						if (fillSamples < state->debug.video[videoOffset].msaa-1 ||
-							fillSamples > state->debug.video[videoOffset].msaa+1) {
-							Assert(false);
-						}*/
 						state->debug.video[VideoMemoryOffset(x, y)].msaa = fillSamples;
 					}
-	#if 0
-					Vec2 p = GetScreenCoords(state, x, y);
-					float lo0 = LineOrientation(v0, v1, p);
-					float lo1 = LineOrientation(v1, v2, p);
-					float lo2 = LineOrientation(v2, v0, p);
-					if (lo0 <= 0 && lo1 <= 0 && lo2 <= 0) {
-						// todo: This means 12 sqrts per pixel! Not great
-						float c0 = TriangleArea(p, v1, v2) / triArea;
-						float c1 = TriangleArea(p, v2, v0) / triArea;
-						float c2 = TriangleArea(p, v0, v1) / triArea;
-						// Color c = ColorLerp({1, 0, 0}, {0, 1, 0}, area0/triArea);
-						Color color = ColorMul(c[0], c0) + ColorMul(c[1], c1) + ColorMul(c[2], c2);
-
-						// float interpX = 1.0f - modf(
-
-						/*float orient = Max3(lo0, lo1, lo2);
-						Vec2 fakeVec = GetPixelCoords(state, -1.0f + orient, 0);
-						float colorMul = 1.0f;
-						if (fakeVec.x > -1.0f) {
-							colorMul = fakeVec.x * -1;
-						}*/
-
-						state->video[VideoMemoryOffset(x, y)] = color/*ColorMul(color, colorMul)*/;
-					}
-	#endif
 				}
 			}
 		}
 
 		v += 3;
+		c += 3;
 	}
 }
 
 void DrawTriangle2D (State *state, float *verts, int vectorSize, int count, float *colors, float rotation = 0.0f) {
-	// Vec2 *v = (Vec2*)verts;
 	Color *c = (Color*)colors;
 
-	// note: Don't use too many vertices or the stack will overflow
-	// todo: Do something better
-	Vec3 *vs = (Vec3*)_alloca(sizeof(Vec3)*count);
+	// todo: Don't malloc please
+	Vec3 *vs = (Vec3*)malloc(sizeof(Vec3)*count);
 	for (int i = 0; i < count; ++i) {
 		vs[i] = {};
 		for (int j = 0; j < vectorSize && j < 3; ++j) {
@@ -382,55 +354,141 @@ void DrawTriangle2D (State *state, float *verts, int vectorSize, int count, floa
 		OrthoProjection(state, &vs[i]);
 	}
 
-	RasterizeTriangle(state, (float*)vs, vectorSize, count, colors, rotation);
+	RasterizeTriangle(state, (float*)vs, vectorSize, count, colors);
+
+	free(vs);
 }
 
-void DrawTriangle3D (State *state, float *verts, int vectorSize, int count, float *colors, float rotation = 0.0f) {
+void EulerMatrixRotation (Vec3 *vec, float xRads, float yRads, float zRads) {
+	// todo: This is horrible. fix it.
+
+	float x[] = {
+		1, 0, 0, 0,
+		0, cosf(xRads), -sinf(xRads), 0,
+		0, sinf(xRads), cosf(xRads), 0,
+		0, 0, 0, 1,
+	};
+
+	float y[] = {
+		 cosf(yRads), 0, sinf(yRads), 0,
+		 0, 1, 0, 0,
+		-sinf(yRads), 0, cosf(yRads), 0,
+		 0, 0, 0, 1,
+	};
+
+	float z[] = {
+		cosf(zRads), -sinf(zRads), 0, 0,
+		sinf(zRads), cosf(zRads), 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1,
+	};
+
+	float xy[] = {
+		x[0]*y[0] + x[1]*y[4] + x[2]*y[8] + x[3]*y[12],
+		x[0]*y[1] + x[1]*y[5] + x[2]*y[9] + x[3]*y[13],
+		x[0]*y[2] + x[1]*y[6] + x[2]*y[10] + x[3]*y[14],
+		x[0]*y[3] + x[1]*y[7] + x[2]*y[11] + x[3]*y[15],
+
+		x[4]*y[0] + x[5]*y[4] + x[6]*y[8] + x[7]*y[12],
+		x[4]*y[1] + x[5]*y[5] + x[6]*y[9] + x[7]*y[13],
+		x[4]*y[2] + x[5]*y[6] + x[6]*y[10] + x[7]*y[14],
+		x[4]*y[3] + x[5]*y[7] + x[6]*y[11] + x[7]*y[15],
+
+		x[8]*y[0] + x[9]*y[4] + x[10]*y[8] + x[11]*y[12],
+		x[8]*y[1] + x[9]*y[5] + x[10]*y[9] + x[11]*y[13],
+		x[8]*y[2] + x[9]*y[6] + x[10]*y[10] + x[11]*y[14],
+		x[8]*y[3] + x[9]*y[7] + x[10]*y[11] + x[11]*y[15],
+
+		x[12]*y[0] + x[13]*y[4] + x[14]*y[8] + x[15]*y[12],
+		x[12]*y[1] + x[13]*y[5] + x[14]*y[9] + x[15]*y[13],
+		x[12]*y[2] + x[13]*y[6] + x[14]*y[10] + x[15]*y[14],
+		x[12]*y[3] + x[13]*y[7] + x[14]*y[11] + x[15]*y[15],
+	};
+
+	float xyz[] = {
+		xy[0]*z[0] + xy[1]*z[4] + xy[2]*z[8] + xy[3]*z[12],
+		xy[0]*z[1] + xy[1]*z[5] + xy[2]*z[9] + xy[3]*z[13],
+		xy[0]*z[2] + xy[1]*z[6] + xy[2]*z[10] + xy[3]*z[14],
+		xy[0]*z[3] + xy[1]*z[7] + xy[2]*z[11] + xy[3]*z[15],
+
+		xy[4]*z[0] + xy[5]*z[4] + xy[6]*z[8] + xy[7]*z[12],
+		xy[4]*z[1] + xy[5]*z[5] + xy[6]*z[9] + xy[7]*z[13],
+		xy[4]*z[2] + xy[5]*z[6] + xy[6]*z[10] + xy[7]*z[14],
+		xy[4]*z[3] + xy[5]*z[7] + xy[6]*z[11] + xy[7]*z[15],
+
+		xy[8]*z[0] + xy[9]*z[4] + xy[10]*z[8] + xy[11]*z[12],
+		xy[8]*z[1] + xy[9]*z[5] + xy[10]*z[9] + xy[11]*z[13],
+		xy[8]*z[2] + xy[9]*z[6] + xy[10]*z[10] + xy[11]*z[14],
+		xy[8]*z[3] + xy[9]*z[7] + xy[10]*z[11] + xy[11]*z[15],
+
+		xy[12]*z[0] + xy[13]*z[4] + xy[14]*z[8] + xy[15]*z[12],
+		xy[12]*z[1] + xy[13]*z[5] + xy[14]*z[9] + xy[15]*z[13],
+		xy[12]*z[2] + xy[13]*z[6] + xy[14]*z[10] + xy[15]*z[14],
+		xy[12]*z[3] + xy[13]*z[7] + xy[14]*z[11] + xy[15]*z[15],
+	};
+
+	float w = 0;
+	Vec3 v = *vec;
+	vec->x = xyz[0]*v.x + xyz[1]*v.y + xyz[2]*v.z + xyz[3]*w;
+	vec->y = xyz[4]*v.x + xyz[5]*v.y + xyz[6]*v.z + xyz[7]*w;
+	vec->z = xyz[8]*v.x + xyz[9]*v.y + xyz[10]*v.z + xyz[11]*w;
+}
+
+void DrawTriangle3D (State *state, float *verts, int vectorSize, int count, float *colors, EulerAngle rotation = {}, bool drawBackFaces = false) {
 	Color *c = (Color*)colors;
 
-	// note: Don't use too many vertices or the stack will overflow
-	// todo: Do something better
-	Vec3 *vs = (Vec3*)_alloca(sizeof(Vec3)*count);
+	// todo: Don't malloc please
+	Vec3 *vs = (Vec3*)malloc(sizeof(Vec3)*count);
 	for (int i = 0; i < count; ++i) {
 		vs[i] = {};
 		for (int j = 0; j < vectorSize && j < 3; ++j) {
 			vs[i].e[j] = verts[i*vectorSize + j];
 		}
-		vs[i].xy = RotateVector(vs[i].xy, rotation);
+
+		Vec3 v = vs[i];
+
+		// float xm = (cosf(rotation.y) + sinf(rotation.y))
+		// 		  *(cosf(rotation.z) + sinf(rotation.z));
+		// float ym = (cosf(rotation.x) + sinf(rotation.x))
+		// 		  *(cosf(rotation.z) - sinf(rotation.z));
+		// float zm = (cosf(rotation.x) - sinf(rotation.x))
+		// 		  *(cosf(rotation.y) - sinf(rotation.y));
+
+		EulerMatrixRotation(&vs[i], rotation.x, rotation.y, rotation.z);
+
+		vs[i].z += 3.0f;
+
 		PerspectiveProjection(state, &vs[i], 70);
 	}
 
-	RasterizeTriangle(state, (float*)vs, vectorSize, count, colors, rotation);
+	char shit[64];
+	sprintf(shit, "v0x %f \n", vs[0].x);
+	OutputDebugString(shit);
+
+	RasterizeTriangle(state, (float*)vs, vectorSize, count, colors);
+
+	if (drawBackFaces) {
+		for (int i = 0; i < count/3; ++i) {
+			Vec3 swap = vs[i*3 + 0];
+			vs[i*3 + 0] = vs[i*3 + 2];
+			vs[i*3 + 2] = swap;
+
+			Color colorSwap = c[i*3 + 0];
+			c[i*3 + 0] = c[i*3 + 2];
+			c[i*3 + 2] = colorSwap;
+		}
+
+		RasterizeTriangle(state, (float*)vs, vectorSize, count, colors);
+	}
+
+	free(vs);
 }
 
-void DrawRectangle (State *state, float *verts, int vectorSize, int count, float *colors, float rotation = 0.0f) {
-#if 0
-	int startX;
-	int startY;
-	int endX;
-	int endY;
-	GetPixelCoords(state, x, y, &startX, &startY);
-	GetPixelCoords(state, x2, y2, &endX, &endY);
-
-	int sx = Min(startX, endX);
-	int ex = Max(startX, endX);
-	int sy = Min(startY, endY);
-	int ey = Max(startY, endY);
-
-	for (int y = sy; y < ey; ++y) {
-		for (int x = sx; x < ex; ++x) {
-			if (y >= 0 && y < state->backBufferSize.y &&
-				x >= 0 && x < state->backBufferSize.x) {
-				state->video[y*state->backBufferSize.x + x] = color;
-			}
-		}
-	}
-#endif
-
+void DrawRectangle (State *state, float *verts, int vectorSize, int count, float *colors, EulerAngle rotation, float rx, float ry, float rz) {
 	int quadVertCount = ((count/4)*4);
 	int triVertCount = quadVertCount + (quadVertCount/2);
 
-	float *v = (float*)_alloca(sizeof(float)*triVertCount*vectorSize);
+	float *v = (float*)malloc(sizeof(float)*triVertCount*vectorSize);
 	float *temp = v;
 	for (int i = 0; i < quadVertCount/4; ++i) {
 		for (int j = 0; j < vectorSize; ++j) {
@@ -455,33 +513,35 @@ void DrawRectangle (State *state, float *verts, int vectorSize, int count, float
 		temp += 6*vectorSize;
 	}
 
-	DrawTriangle3D(state, v, vectorSize, triVertCount, NULL, rotation);
+	float *c = (float*)malloc(sizeof(float)*triVertCount*4);
+	temp = c;
+	for (int i = 0; i < quadVertCount/4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			temp[0*4 + j] = colors[(i*4*4) + 0*4 + j];
+		}
+		for (int j = 0; j < 4; ++j) {
+			temp[1*4 + j] = colors[(i*4*4) + 1*4 + j];
+		}
+		for (int j = 0; j < 4; ++j) {
+			temp[2*4 + j] = colors[(i*4*4) + 2*4 + j];
+		}
+		for (int j = 0; j < 4; ++j) {
+			temp[3*4 + j] = colors[(i*4*4) + 0*4 + j];
+		}
+		for (int j = 0; j < 4; ++j) {
+			temp[4*4 + j] = colors[(i*4*4) + 2*4 + j];
+		}
+		for (int j = 0; j < 4; ++j) {
+			temp[5*4 + j] = colors[(i*4*4) + 3*4 + j];
+		}
 
-	// Vec2 *v = (Vec2*)verts;
-	// Color *c = (Color*)colors;
+		temp += 6*4;
+	}
 
-	// Vec2 verts0[] = {
-	// 	v[0],
-	// 	v[1],
-	// 	v[2],
-	// };
-	// Color colors0[] = {
-	// 	c[0],
-	// 	c[1],
-	// 	c[2],
-	// };
-	// Vec2 verts1[] = {
-	// 	v[0],
-	// 	v[2],
-	// 	v[3],
-	// };
-	// Color colors1[] = {
-	// 	c[0],
-	// 	c[2],
-	// 	c[3],
-	// };
-	// DrawTriangle2D(state, (float*)verts0, (float*)colors0, rotation);
-	// DrawTriangle2D(state, (float*)verts1, (float*)colors1, rotation);
+	DrawTriangle3D(state, v, vectorSize, triVertCount, c, rotation);
+
+	free(v);
+	free(c);
 }
 
 void DrawPoint (State *state, float x, float y, Color color) {
@@ -516,18 +576,12 @@ void Update (State *state) {
 	state->lastTime = time;
 	double dt = state->dt;
 
-	// Color red = {255, 0, 0, 255};
-	/*for (int i = 0; i < 640*360; ++i) {
-		// state->video[i] = {RandomFloat(), RandomFloat(), RandomFloat(), 255};
-		state->video[i] = {0, 1, 0, 1};
-	}*/
-
-	// DrawRectangle(state, -0.5f, 0.5f, 0.5f, -0.5f, {0, 1, 0, 1});
-
 	ClearBackBuffer(state, {0, 0, 0, 0});
 
-	static float rotation = 0.0f;
-	rotation += 0.2f * dt;
+	static EulerAngle rotation = {};
+	rotation.x += 0.4f * dt;
+	rotation.y += 0.8f * dt;
+	// rotation.z += 0.4f * dt;
 
 	// if (state->input.rightDown) {
 	// 	rotation += 0.0002f /** dt*/;
@@ -536,8 +590,9 @@ void Update (State *state) {
 	// 	rotation -= 0.0002f /** dt*/;
 	// }
 
+#if 0
 	float triVerts[] = {
-		0.0f, 0.5f,
+		-0.5f, 0.5f,
 		1.0f, 0.0f,
 		0.25, -0.5f,
 
@@ -554,42 +609,96 @@ void Update (State *state) {
 		0, 1, 1, 0,
 		1, 1, 0, 0,
 	};
-	// DrawTriangle2D(state, triVerts, 2, 6, colors, rotation);
+	DrawTriangle3D(state, triVerts, 2, 3, colors, rotation);
+#endif
+
+#if 0
+	float triVerts2[] = {
+		-0.5f, 0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+
+		-0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f,
+	};
+	float colors2[] = {
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+
+		1, 0, 0, 0,
+		0, 0, 1, 0,
+		1, 1, 0, 0,
+	};
+	DrawTriangle3D(state, triVerts2, 3, 6, colors2, rotation);
+#endif
 
 	float quadVerts[] = {
-		-0.5f, 0.5f, -10.0f,
-		0.5f, 0.5f, -10.0f,
-		0.5f, -0.5f, -10.0f,
-		-0.5f, -0.5f, -10.0f,
-
-		/*-0.5f, 0.0f, 0.5f,
-		0.5f, 0.0f, 0.5f,
-		0.5f, 0.0f, -0.5f,
-		-0.5f, 0.0f, -0.5f,
-
+		// front
+		-0.5f, 0.5f, -0.5f,
+		0.5f, 0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, -0.5f,
+		// back
+		0.5f, 0.5f, 0.5f,
+		-0.5f, 0.5f, 0.5f,
 		-0.5f, -0.5f, 0.5f,
+		0.5f, -0.5f, 0.5f,
+		// left
 		-0.5f, 0.5f, 0.5f,
 		-0.5f, 0.5f, -0.5f,
-		-0.5f, -0.5f, -0.5f,*/
+		-0.5f, -0.5f, -0.5f,
+		-0.5f, -0.5f, 0.5f,
+		// right
+		0.5f, 0.5f, -0.5f,
+		0.5f, 0.5f, 0.5f,
+		0.5f, -0.5f, 0.5f,
+		0.5f, -0.5f, -0.5f,
+		// top
+		-0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, 0.5f,
+		0.5f, 0.5f, -0.5f,
+		-0.5f, 0.5f, -0.5f,
+		// bottom
+		-0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, -0.5f,
+		0.5f, -0.5f, 0.5f,
+		-0.5f, -0.5f, 0.5f,
 	};
 	float quadColors[] = {
-		1, 1, 0, 0,
-		0, 1, 1, 0,
-		1, 0, 1, 0,
-		1, 1, 1, 0,
+		1, 0, 0, 0,
+		1, 0, 0, 0,
+		1, 0, 0, 0,
+		1, 0, 0, 0,
+
+		0, 1, 0, 0,
+		0, 1, 0, 0,
+		0, 1, 0, 0,
+		0, 1, 0, 0,
+
+		0, 0, 1, 0,
+		0, 0, 1, 0,
+		0, 0, 1, 0,
+		0, 0, 1, 0,
 
 		1, 1, 0, 0,
+		1, 1, 0, 0,
+		1, 1, 0, 0,
+		1, 1, 0, 0,
+
 		0, 1, 1, 0,
+		0, 1, 1, 0,
+		0, 1, 1, 0,
+		0, 1, 1, 0,
+
 		1, 0, 1, 0,
-		1, 1, 1, 0,
+		1, 0, 1, 0,
+		1, 0, 1, 0,
+		1, 0, 1, 0,
 	};
-	DrawRectangle(state, quadVerts, 3, 4, quadColors, rotation);
-
-	/*static Vec2 pos = {};
-	pos.x = 0.5f*cosf(rotation) + 0.5f*sinf(rotation);
-	pos.y = 0.5f*cosf(rotation) - 0.5f*sinf(rotation);
-	DrawPoint(state, pos.x, pos.y, {1, 0, 0, 0});*/
-
+	DrawRectangle(state, quadVerts, 3, 24, quadColors, rotation, rotation.x, rotation.y, rotation.z);
+	
 #if 0
 	for (int i = 0; i < state->backBufferSize.x*state->backBufferSize.y; ++i) {
 		float c = state->debug.video[i].msaa / 4.0f;
